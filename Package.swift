e7df64de76d7ccb29805fa15bdad6b64ba10e27d
neelio <neelio@users.noreply.github.com>
2019-04-13 10:30:55 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["FileReader"]),
     ],
     dependencies: [
-        .package(url:"https://github.com/<repoaccount>/<reponame>", from: "1.0.0"),
+        //
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
